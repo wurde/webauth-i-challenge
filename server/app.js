eacec9af0460282000ee85e3e5fc64fc7376013e
Andy Bettisworth <andy@accreu.com>
2019-06-10 16:46:23 -0500

    Define error handler for server

--- server/app.js
+++ server/app.js
@@ -12,6 +12,7 @@ const helmet = require('helmet')
  */
 
 const port = process.env.PORT || 8080
+const env = process.env.NODE_ENV || 'development'
 
 /**
  * Define app
@@ -33,6 +34,28 @@ app.use(express.json())
 app.use('/', require('./routes/root_router'))
 app.use('/users', require('./routes/users_router'))
 app.use('/restricted', require('./routes/restricted_router'))
+
+/**
+ * Error handlers
+ */
+
+app.use((req, res, next) => {
+  let err = new Error()
+  err.status  = 404
+  err.message = 'Not found'
+
+  next(err)
+})
+
+app.use((err, req, res, next) => {
+  err.status = (err.status || 500)
+  err.message = (err.message || 'Internal Server Error')
+  if (env === 'production') { err.stack = undefined }
+
+  console.error(err.message)
+
+  res.status(err.status).json({ errors: { status: err.status, message: err.message } })
+})
 
 /**
  * Start server
