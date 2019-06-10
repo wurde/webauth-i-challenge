0372616ebe10c1727043ac564a2a337e84c3d11f
Andy Bettisworth <andy@accreu.com>
2019-06-10 15:09:12 -0500

    Add User model

---
+++ server/models/User.js
@@ -0,0 +1,23 @@
+'use strict'
+
+/**
+ * Dependencies
+ */
+
+const db_client = require('../db/client')
+
+/**
+ * Define model
+ */
+
+class User {
+  static async all() {
+    return []
+  }
+}
+
+/**
+ * Export model
+ */
+
+module.exports = User
