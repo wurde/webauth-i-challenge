b96c8008163c52324251de80ef00bfbf41a375e0
Andy Bettisworth <andy@accreu.com>
2019-06-11 17:52:52 -0500

    deps: install @material-ui/core

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -8,21 +8,45 @@ import React from 'react'
 import { LoginStyle } from './styles/index'
 
 /**
+ * Constants
+ */
+
+const Component = React.Component
+
+/**
  * Define component
  */
 
-function Login(props) {
-  return (
-    <LoginStyle>
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-10 col-md-8 col-lg-5">
-            Login
+class Login extends Component {
+  constructor() {
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault()
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  render() {
+    return (
+      <LoginStyle>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-10 col-md-8 col-lg-5">
+
+              <form onSubmit={handleOnSubmit}>
+                <input type=
+              </form>
+
+            </div>
           </div>
         </div>
-      </div>
-    </LoginStyle>
-  )
+      </LoginStyle>
+    )
+  }
 }
 
 /**
